e2bcc65eaaa0c6eb328c27ac501383aafc660954
itc-s23024 <s23024@std.it-college.ac.jp>
2024-10-24 14:55:43 +0900

    直してないけど完成

--- app/src/main/java/jp/ac/it_college/std/s23024/myanimation/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23024/myanimation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import jp.ac.it_college.std.s23024.myanimation.ui.TutorialPager
 import jp.ac.it_college.std.s23024.myanimation.ui.theme.MyAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAnimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TutorialPager(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyAnimationTheme {
-        Greeting("Android")
     }
 }
